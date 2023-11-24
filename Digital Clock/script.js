60b12cfbd57026d5cba25105a06ecbeac3cddb01
Ameer khan B <106725517+itisameerkhan@users.noreply.github.com>
2023-11-24 09:28:45 +0530

    24/11/2023 09:28

---
+++ Digital Clock/script.js
@@ -0,0 +1,20 @@
+let displayTime = function () {
+    let dateTime = new Date();
+    let hr = dateTime.getHours();
+    let min = padZero(dateTime.getMinutes());
+    let sec = padZero(dateTime.getSeconds());
+    if (hr > 12) {
+      hr = hr - 12;
+      document.getElementById("ampm").innerHTML = "PM";
+    } else {
+      document.getElementById("ampm").innerHTML = "AM";
+    }
+    document.getElementById("hours").innerHTML = padZero(hr);
+    document.getElementById("mins").innerHTML = min;
+    document.getElementById("seconds").innerHTML = sec;
+  };
+  let padZero = function (num) {
+    return num < 10 ? "0" + num : num;
+  };
+  setInterval(displayTime, 500);
+  
